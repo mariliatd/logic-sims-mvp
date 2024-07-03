3646811a559ec27ac19738022742fa66445d31e7
mariliatd <mariliatd@gmail.com>
2024-07-02 21:03:12 -0300

    fix: use slot instead of components

--- src/components/SimPageTemplate.vue
+++ src/components/SimPageTemplate.vue
@@ -2,19 +2,27 @@
   <div class="template-box">
     <v-app>
       <v-row>
-        <v-col cols="8"><SimWorkspace /></v-col>
-        <v-col cols="4"><Pseudocode /></v-col>
+        <v-col cols="8">
+          <div class="sim-box">
+            <slot name="workspace"></slot>
+          </div>
+        </v-col>
+        <v-col cols="4">
+          <div class="code-box">
+            <p class="sub-title">Pseudocódigo</p>
+            <slot name="pseudocode"></slot>
+          </div>
+        </v-col>
       </v-row>
     </v-app>
   </div>
 </template>
 
-<script setup lang="ts">
-import Pseudocode from "./Pseudocode.vue";
-import SimWorkspace from "./SimWorkspace.vue";
-</script>
+<script setup lang="ts"></script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+
 * {
   margin: 0;
   padding: 0;
@@ -36,5 +44,26 @@ body {
   border: solid 0.12rem #1d2d44;
   border-radius: 1rem;
   overflow: hidden;
+}
+
+.sim-box {
+  margin: 1.2rem;
+}
+
+.code-box {
+  border: solid 0.12rem #1d2d44;
+  border-radius: 1rem;
+  margin: 1.2rem;
+}
+
+.sub-title {
+  font-family: "Roboto", sans-serif;
+  font-size: 1.4rem;
+  text-align: center;
+  color: #1d2d44;
+  background-color: #eccbdf;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  margin: 1rem;
 }
 </style>
--- src/components/SimWorkspace.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="sim-box"></div>
-</template>
-
-<style>
-.sim-box {
-  margin: 1.2rem;
-}
-</style>
