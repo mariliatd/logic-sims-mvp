3a4356efb1e181bef0bbc7524826bb4e82791ad9
mariliatd <mariliatd@gmail.com>
2024-08-12 22:21:00 -0300

    feat: add output dialog component

---
+++ src/components/OutputDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog v-model="openOutputDialog" width="300px">
+    <v-card :text="content">
+      <v-card-actions>
+        <v-btn text="Fechar" @click="close"></v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    openOutputDialog: { type: Boolean, required: true },
+    content: { type: String, required: true },
+  },
+  methods: {
+    close() {
+      this.$emit("closeOutputDialog", false);
+    },
+  },
+});
+</script>
